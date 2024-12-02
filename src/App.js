13b20298bdd41e6580ac0b2e0d5eff4c78390db7
DESKTOP-KMMN8IV\USER <hwangyujin22@gmail.com>
2024-12-03 00:06:48 +0900

    로고 변경 테스트

--- src/App.js
+++ src/App.js
@@ -1,11 +1,12 @@
-import logo from './logo.svg';
+import React from 'react';
+import newLogo from './ootachi.png'; // 새 이미지를 가져오기
 import './App.css';
 
 function App() {
   return (
     <div className="App">
       <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
+        <img src={newLogo} className="App-logo" alt="logo" />
         <p>
           Edit <code>src/App.js</code> and save to reload.
         </p>
